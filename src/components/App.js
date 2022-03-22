f5531f22f26dae34796c94c44568568ebcdd1023
marikomands <marikomands@gmail.com>
2022-03-22 10:22:47 +0000

    Added BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return (
+    <div>
+      PageOne
+      <Link to="/pagetwo">Navigate to Page Two</Link>
+    </div>
+  );
+};
+console.log("🚀 ~ PageOne ~ PageOne", PageOne);
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <button>click me</button>
+      <Link to="/">Navigate to Page One</Link>
+    </div>
+  );
+};
+console.log("🚀 ~ PageTwo ~ PageTwo", PageTwo);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Route path="/" exact component={PageOne} />
+        <Route path="/pagetwo" component={PageTwo} />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
