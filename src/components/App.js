bc74b133b8de246a1a9f580dc2175b715753c414
marikomands <marikomands@gmail.com>
2022-03-22 11:27:36 +0000

    deleted page route created 5 new route

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
-console.log("🚀 ~ PageOne ~ PageOne", PageOne);
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>click me</button>
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
-console.log("🚀 ~ PageTwo ~ PageTwo", PageTwo);
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" component={PageTwo} />
-      </div>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
