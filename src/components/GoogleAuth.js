13f205ce0dd90b942d2c95274ec770690b52bc55
marikomands <marikomands@gmail.com>
2022-03-24 13:56:44 +0000

    added setState() and listen function

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,17 +1,40 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "546145138944-4m2785mr3v73cm1325ci1pk53ao084n4.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          // ↑initialize client by call back function  load method only allows call back function
+          clientId:
+            "546145138944-4m2785mr3v73cm1325ci1pk53ao084n4.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if you are signed in</div>;
+    } else if (this.state.isSignedIn === true) {
+      return <div>You are signed in!</div>;
+    } else {
+      return <div>You are not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
