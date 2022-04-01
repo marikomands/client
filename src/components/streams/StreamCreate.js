fd404639addab9f3b4f476a9daef30424fe7a963
marikomands <marikomands@gmail.com>
2022-04-01 11:36:44 +0100

    imported reduxForm and make use of it

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  render() {
+    console.log(this.props);
+    return <div>StreamCreate</div>;
+  }
+}
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  // ↑the purpose of this form
+})(StreamCreate);
