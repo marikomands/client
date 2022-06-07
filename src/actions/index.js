64bc67f2bf99e2eb75894e07dd002a846ba45f26
marikomands <marikomands@gmail.com>
2022-06-07 13:12:22 +0100

    added history.push("/") after deleting

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import { useHistory } from "react-router-dom";
 import streams from "../apis/streams";
 import history from "../history";
 
@@ -36,7 +37,7 @@ export const fetchStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
   dispatch({ type: "EDIT_STREAM", payload: response.data });
   history.push("/");
 };
@@ -44,4 +45,5 @@ export const editStream = (id, formValues) => async (dispatch) => {
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
   dispatch({ type: "DELETE_STREAM", payload: id });
+  history.push("/");
 };
